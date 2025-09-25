9e4c96d10fd52fffddf14f4b6ab56d1035f4893c
jw <bau12288@gmail.com>
2025-09-25 16:00:20 +0900

    큰수 출력

---
+++ src/main/java/algorithm/demo/num/Main02.java
@@ -0,0 +1,22 @@
+package algorithm.demo.num;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main02 {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args){
+        algorithm.demo.num.Main T = new algorithm.demo.num.Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
